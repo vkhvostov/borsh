4efa032e3149a0a41208125905b8e63afb56fc3a
vkhvostov <30327231+vkhvostov@users.noreply.github.com>
2025-04-18 13:08:30 +0200

    [borsh] Add librariers in the main header file (#4)

--- includes/borsh.h
+++ includes/borsh.h
@@ -1,8 +1,41 @@
-#ifndef PHILO_H
-# define PHILO_H
+#ifndef BORSH_H
+# define BORSH_H
 
+// printf, perror
 # include <stdio.h>
+// malloc, free, exit
 # include <stdlib.h>
+// access, read, write, close, fork, execve, getcwd, chdir, dup, dup2, isatty
+// ttyname, ttyslot
+# include <unistd.h>
+// open, O_* flags
+# include <fcntl.h>
+// strerror
+# include <string.h>
+// stat, lstat, fstat
+# include <sys/stat.h>
+// wait, waitpid, wait3, wait4, stat, lstat, fstat, opendir, readdir
+# include <sys/types.h>
+// wait, waitpid, wait3, wait4
+# include <sys/wait.h>
+// opendir, readdir, closedir
+# include <dirent.h>
+// signal, sigaction, sigemptyset, sigaddset, kill
+# include <signal.h>
+// tcsetattr, tcgetattr
+# include <termios.h>
+// tgetent (sometimes required by curses), might be optional
+# include <curses.h>
+// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <term.h>
+// ioctl
+# include <sys/ioctl.h>
+// readline, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay
+# include <readline/readline.h>
+// add_history
+# include <readline/history.h>
+// errno (for error handling if needed)
+# include <errno.h>
 
 
 
