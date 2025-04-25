f2f2f2f0bf178f9fa4b127ed11d89faf0182f8c8
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2025-04-25 11:24:47 +0200

    #23 added function for handling signals & disabled echoing of control chars
    * added function for handling signals & disabled echoing of control chars
    
    * Fix compilation and delete minishell binary
    
    * Update src/main.c
    
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>
    
    * Update src/main.c
    
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>
    
    * add comment for CTR+C function; change comments position according to the norm
    
    * o. files deleted
    
    * deleted minishell && added static to function
    
    ---------
    
    Co-authored-by: vkhvostov <viktor.khvostov@gmail.com>
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include "../include/borsh.h"
 
-int read_input(void)
+static void	hide_ctrl_c_echo(void)
+{
+	struct termios	term;
+	// get current terminal settings
+	tcgetattr(STDIN_FILENO, &term);
+	// disable echoing of control chars
+	term.c_lflag &= ~ECHOCTL;
+	// apply the settings
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+// handling CTRL+C
+static void	handle_sigint(int sig)
+{
+	(void)sig;
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+int	read_input(void)
 {
 	char *line;
 
@@ -17,10 +38,13 @@ int read_input(void)
 	return 0;
 }
 
-int main(int count, char **args)
+int	main(int count, char **args)
 {
 	if (count == 0 && args)
 		return (1);
+	hide_ctrl_c_echo();
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 	read_input();
 	return (0);
 }
