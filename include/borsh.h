d11f1c37a677216dfd05f165bab50c0aead78ba3
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2025-04-25 16:16:08 +0200

    #26 created token struct and defined token types

--- include/borsh.h
+++ include/borsh.h
@@ -39,6 +39,19 @@
 // errno (for error handling if needed)
 # include <errno.h>
 
+typedef enum	e_token_type {
+	T_WORD,
+	T_PIPE,
+	T_REDIR_IN,
+	T_REDIR_OUT,
+	T_REDIR_APPEND,
+	T_HEREDOC
+}	t_token_type;
 
+typedef struct	s_token {
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
 
 #endif
