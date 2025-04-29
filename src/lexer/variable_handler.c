729891fe4c906b1b08cd76f09c1171c8b8cec902
vkhvostov <viktor.khvostov@gmail.com>
2025-04-29 17:53:33 +0200

    #25 More work on norms and structuring

--- src/lexer/variable_handler.c
+++ src/lexer/variable_handler.c
@@ -13,6 +13,37 @@ static char *get_var_value(const char *name)
 		return ft_strdup("");
 }
 
+/**
+ * This function is called when we encounter a variable
+ * in the input string. It extracts the variable name,
+ * retrieves its value, and appends it to the result.
+ * The input string is assumed to be a valid C string.
+ */
+static size_t extract_and_expand_var(const char *input, size_t i, char **result)
+{
+	size_t	var_len;
+	char	*var;
+	char	*val;
+
+	var_len = 0;
+	while (ft_isalnum(input[i + var_len]) || input[i + var_len] == '_')
+		var_len++;
+	var = strndup(&input[i], var_len);
+	val = get_var_value(var);
+	*result = ft_realloc(*result, ft_strlen(*result) + ft_strlen(val) + 1);
+	if (*result == NULL)
+	{
+		free(var);
+		free(val);
+		return (-1);
+	}
+	ft_strlcpy(*result, *result, ft_strlen(*result) + 1);
+	ft_strlcat(*result, val, ft_strlen(*result) + ft_strlen(val) + 1);
+	free(var);
+	free(val);
+	return (var_len);
+}
+
 char *expand_variables(const char *input)
 {
 	char *result = malloc(1);
@@ -28,33 +59,22 @@ char *expand_variables(const char *input)
 			in_double = !in_double;
 		else if (input[i] == '$' && !in_single) {
 			i++;
-			size_t var_len = 0;
-
 			if (input[i] == '?') {
 				char *val = get_var_value("?");
-				result = realloc(result, strlen(result) + strlen(val) + 1);
+				result = ft_realloc(result, strlen(result) + strlen(val) + 1);
 				strcat(result, val);
 				free(val);
 				i++;
 				continue;
 			}
 
-			while (isalnum(input[i + var_len]) || input[i + var_len] == '_')
-				var_len++;
-
-			char *var = strndup(&input[i], var_len);
-			char *val = get_var_value(var);
-			result = realloc(result, strlen(result) + strlen(val) + 1);
-			strcat(result, val);
-			free(var);
-			free(val);
-			i += var_len;
+			i += extract_and_expand_var(input, i, &result);
 			continue;
 		}
 
 		// Append regular character
 		size_t len = strlen(result);
-		result = realloc(result, len + 2);
+		result = ft_realloc(result, len + 2);
 		result[len] = input[i];
 		result[len + 1] = '\0';
 		i++;
