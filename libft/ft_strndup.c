7adf7fe45b00d35c3ac5767c6d8884de79a3427b
Daryna Bilenko <104099471+DarynaBb@users.noreply.github.com>
2025-05-02 10:52:30 +0200

    #29 Function ft_strndup() added
    * ft_strndup() added
    
    * New line at the end
    
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>
    
    * Update libft/ft_strndup.c
    
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: vkhvostov <30327231+vkhvostov@users.noreply.github.com>

---
+++ libft/ft_strndup.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strndup(char *s, size_t n)
+{
+	size_t	i;
+	char	*dup;
+
+	i = 0;
+	while (s[i] && i < n)
+		i++;
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i] && i < n)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
