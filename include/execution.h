9eca632f9a2b047f6b78d0565e9ac4c1deab1719
vkhvostov <viktor.khvostov@gmail.com>
2025-06-09 19:37:14 +0200

    Remove unused declarations

--- include/execution.h
+++ include/execution.h
@@ -18,8 +18,5 @@ pid_t launch_process(t_command *command, t_process_params params);
 char *resolve_path(char *command_name);
 int handle_redirections(t_command *command, int *in_fd, int *out_fd);
 int handle_heredoc(t_redirect *redir, int *heredoc_pipe_fd);
-void wait_for_processes(pid_t *pids, int count);
-void setup_pipes(t_command *commands, int num_commands, int (*pipes)[2]);
-void close_pipes(int num_pipes, int (*pipes)[2]);
 
 #endif
