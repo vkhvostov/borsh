274dc78a9b6cd47699f1f094a5355586aa19af45
vkhvostov <viktor.khvostov@gmail.com>
2025-06-22 17:49:58 +0200

    Fix cd with too many arguments for linux

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -4,13 +4,18 @@ int	builtin_cd(char **argv)
 {
 	char	*target;
 
+	if (argv[2] != NULL)
+	{
+		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
+		return (1);
+	}
 	if (argv[1] == NULL || (ft_strcmp(argv[1], "~") == 0))
 		target = getenv("HOME");
 	else
 		target = argv[1];
 	if (target == NULL)
 	{
-		write(2, "cd: HOME not set\n", 17);
+		ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
 		return (1);
 	}
 	if (chdir(target) != 0)
