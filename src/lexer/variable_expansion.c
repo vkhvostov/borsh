5e26ff9cef834d3f23b85e2bdcb7cd9604b7b33d
vkhvostov <viktor.khvostov@gmail.com>
2025-07-05 19:01:16 +0200

    Support single digit environment variable expansion

--- src/lexer/variable_expansion.c
+++ src/lexer/variable_expansion.c
@@ -72,13 +72,38 @@ static int	expand_special_var(char **result, char **env, int *exit_status)
 	return (0);
 }
 
-// handler for variable expansion
-// determines whether to expand a regular variable, a special variable ($?),
-// or treat the '$' as a literal character
-int	process_expansion(t_var_ctx *ctx)
+// expands a numeric variable ($1, $2, etc)
+static int	expand_numeric_var(t_var_ctx *ctx)
 {
-	size_t	consumed;
+	char	*variable;
+	char	*value;
+	size_t	total_len;
 
+	variable = ft_strndup(&ctx->input[ctx->i], 1);
+	if (!variable)
+		return (-1);
+	value = get_variable_value(variable, ctx->env, ctx->exit_status);
+	free(variable);
+	if (!value)
+		return (-1);
+	if (append_var_value(ctx->result, value) == -1)
+	{
+		free(value);
+		return (-1);
+	}
+	free(value);
+	total_len = 1;
+	while (ft_isdigit(ctx->input[ctx->i + total_len]))
+	{
+		if (append_chars(ctx->input, ctx->i + total_len, ctx->result) == -1)
+			return (-1);
+		total_len++;
+	}
+	return (total_len);
+}
+
+static int	should_expand_variable(t_var_ctx *ctx)
+{
 	if (!ctx->input[ctx->i + 1] || (ctx->input[ctx->i + 1] != '?'
 			&& !ft_isalnum(ctx->input[ctx->i + 1])
 			&& ctx->input[ctx->i + 1] != '_'))
@@ -89,11 +114,31 @@ int	process_expansion(t_var_ctx *ctx)
 		return (0);
 	}
 	ctx->i++;
+	return (1);
+}
+
+// handler for variable expansion
+// determines whether to expand a regular variable, a special variable ($?),
+// a numeric variable ($1, $2, etc), or treat the '$' as a literal character
+int	process_expansion(t_var_ctx *ctx)
+{
+	size_t	consumed;
+
+	if (should_expand_variable(ctx) == 0)
+		return (0);
 	if (ctx->input[ctx->i] == '?')
 	{
 		if (expand_special_var(ctx->result, ctx->env, ctx->exit_status) == -1)
 			return (-1);
 		ctx->i++;
+		return (0);
+	}
+	if (ft_isdigit(ctx->input[ctx->i]))
+	{
+		consumed = expand_numeric_var(ctx);
+		if (consumed == (size_t)-1)
+			return (-1);
+		ctx->i += consumed;
 		return (0);
 	}
 	consumed = expand_var(ctx);
--- src/lexer/variable_utils.c
+++ src/lexer/variable_utils.c
@@ -4,9 +4,18 @@ char	*get_variable_value(char *name, char **env, int *exit_status)
 {
 	int		i;
 	int		name_len;
+	char	single_digit[2];
 
 	if (ft_strncmp(name, "?", 2) == 0)
 		return (ft_itoa(*exit_status));
+	if (ft_isdigit(name[0]))
+	{
+		single_digit[0] = name[0];
+		single_digit[1] = '\0';
+		if (single_digit[0] == '0')
+			return (ft_strdup("borsh"));
+		return (ft_strdup(""));
+	}
 	if (!env)
 		return (ft_strdup(""));
 	name_len = ft_strlen(name);
