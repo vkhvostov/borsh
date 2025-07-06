19b69651ab9442387f6382172da6aca845b70389
vkhvostov <viktor.khvostov@gmail.com>
2025-07-06 18:44:05 +0200

    Clean up pids

--- src/execution/execution_utils2.c
+++ src/execution/execution_utils2.c
@@ -35,6 +35,7 @@ void	free_and_exit(t_cmd_ctx *ctx, int exit_status)
 	if (!ctx)
 		return ;
 	free_commands(ctx->cmd);
-	free_shell_env(*(ctx->env));
+	free_shell_env(*ctx->env);
+	free(ctx->pids);
 	exit(exit_status);
 }
